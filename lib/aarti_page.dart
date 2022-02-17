391592e185b12b945d2dd9cfb580447714f6b75e
chief <surajpadhi01@gmail.com>
2022-02-17 23:43:00 +0530

    added audio section

--- lib/aarti_page.dart
+++ lib/aarti_page.dart
@@ -1,3 +1,5 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
@@ -17,8 +19,9 @@ class AartiPage extends StatelessWidget {
         child: Scaffold(
             body: Row(children: [
       Expanded(
+        flex: 3,
         child: Container(
-          width: MediaQuery.of(context).size.width * 0.25,
+          // width: MediaQuery.of(context).size.width * 0.25,
           color: Colors.white,
           child: ListView.builder(
               shrinkWrap: true,
@@ -28,11 +31,11 @@ class AartiPage extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: InkWell(
                     onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AartiSection(),
-                          ));
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) => AartiSection(),
+                      //     ));
                     },
                     child: Card(
                       elevation: 7.0,
@@ -44,38 +47,40 @@ class AartiPage extends StatelessWidget {
                       // margin: const EdgeInsets.all(16),
                       child: Container(
                         padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                color: Colors.white,
-                              ),
-                              height: 80,
-                              width: 80.0,
-                              child: CircleAvatar(
-                                // radius: 16,
-                                backgroundColor: Colors.white,
-                                backgroundImage: AssetImage(
-                                  artis[index].imagePath,
+                        child: Expanded(
+                          child: Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  color: Colors.white,
+                                ),
+                                height: 80,
+                                width: 80.0,
+                                child: CircleAvatar(
+                                  // radius: 16,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: AssetImage(
+                                    artis[index].imagePath,
+                                  ),
                                 ),
                               ),
-                            ),
-
-                            const SizedBox(
-                              width: 25.0,
-                            ),
-                            // RichText(text: TextSpan())
-                            Text(
-                              artis[index].name,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.black,
+                        
+                              const SizedBox(
+                                width: 25.0,
                               ),
-                            ),
-                          ],
+                              // RichText(text: TextSpan())
+                              Text(
+                                artis[index].name,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -89,15 +94,21 @@ class AartiPage extends StatelessWidget {
         thickness: 0.3,
         color: Colors.black,
       ),
-      SizedBox(
-        width: MediaQuery.of(context).size.width * 0.5,
+      Expanded(
+        flex: 4,
+        // width: MediaQuery.of(context).size.width * 0.5,
         child: AartiDetails(),
       ),
       const VerticalDivider(
         thickness: 0.5,
         color: Colors.black,
       ),
-      Container(width: MediaQuery.of(context).size.width * 0.2),
+      Expanded(
+        flex: 3,
+        child: Container( child:MyHomePage('lib/assets/assets_music_music1.mp3')
+
+        ) ,
+        ),
 
       // ]),
     ])));
@@ -148,8 +159,8 @@ class AartiSection extends StatelessWidget {
         child: Scaffold(
             body: Row(children: [
       Expanded(
+        flex: 3,
         child: Container(
-          width: MediaQuery.of(context).size.width * 0.25,
           color: Colors.white,
           child: ListView.builder(
               shrinkWrap: true,
@@ -220,8 +231,8 @@ class AartiSection extends StatelessWidget {
         thickness: 0.3,
         color: Colors.black,
       ),
-      SizedBox(
-        width: MediaQuery.of(context).size.width * 0.5,
+      Expanded(
+       flex: 4,
         child: AartiDetails(),
       ),
 
@@ -229,9 +240,161 @@ class AartiSection extends StatelessWidget {
         thickness: 0.5,
         color: Colors.black,
       ),
-      Container(width: MediaQuery.of(context).size.width * 0.2),
+      Expanded(flex: 3,child:MyHomePage('lib/assets/assets_music_music1.mp3') ,),
 
-      // ]),
+
     ])));
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  String link;
+  MyHomePage(this.link);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+
+    with SingleTickerProviderStateMixin {
+  late AnimationController
+      iconController; 
+
+  bool isAnimated = false;
+  bool showPlay = true;
+  bool shopPause = false;
+  AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
+  @override
+  void initState() {
+    
+    // TODO: implement initState
+    super.initState();
+
+    iconController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    audioPlayer.open(Audio(widget.link),autoStart: false,showNotification: true);
+    final Duration position = audioPlayer.currentPosition.value;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return 
+        
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 10,),
+              ListView.builder(
+                 shrinkWrap: true,
+                itemCount: artis.length,
+                itemBuilder: (context, index){
+        
+               return  Expanded(
+                 child: Card(
+                    
+                             
+                     elevation: 7.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            
+                          ),
+                          color: const Color(0xffFFF3E2),
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          
+                               Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.white,
+                                    ),
+                                    height: 80,
+                                    width: 80.0,
+                                    child: CircleAvatar(
+                                      // radius: 16,
+                                      backgroundColor: Colors.white,
+                                      backgroundImage: AssetImage(
+                                        artis[0].imagePath,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 45,),
+                                  Column(
+                                    children: [
+                                                Text('Shiv arti',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.amber),),
+                                  
+                        
+                         Row(
+                           children: [
+                             InkWell(child: Icon(CupertinoIcons.backward_fill),onTap: (){
+                           audioPlayer.seekBy(Duration(seconds: -10));
+                         },),
+                          GestureDetector(
+                            onTap: () {
+                              AnimateIcon();
+                            },
+                            child: AnimatedIcon(
+                              icon: AnimatedIcons.play_pause,
+                              progress: iconController,
+                              size: 50,
+                              color: Colors.black,
+                            ),
+                          ),
+                          InkWell(child: Icon(CupertinoIcons.forward_fill),onTap: (){
+                            audioPlayer.seekBy(Duration(seconds: 10));
+                            audioPlayer.seek(Duration(seconds: 10));
+                            audioPlayer.next();
+                          },),
+                           ],
+                         )
+                                    ],
+                                  )
+                         
+                        ],
+                      ),
+                    ),
+                  ),
+               );
+                }
+              ),
+            ],
+          ),
+        );
+       
+            
+     
+        
+  }
+
+  void AnimateIcon() {
+    setState(() {
+      isAnimated = !isAnimated;
+
+     if(isAnimated)
+       {
+         iconController.forward();
+         audioPlayer.play();
+       }else{
+       iconController.reverse();
+       audioPlayer.pause();
+     }
+
+
+    });
+  }
+  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    iconController.dispose();
+    audioPlayer.dispose();
+    super.dispose();
+  }
+}
+
+
