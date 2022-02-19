e0a7c8aff6841a613f4e54310fa320d26acd9479
chief <surajpadhi01@gmail.com>
2022-02-19 12:35:22 +0530

    solved indexing problem

--- lib/drawer/configration/Controler.dart
+++ lib/drawer/configration/Controler.dart
@@ -1,11 +1,8 @@
 import 'package:book_app_chief_suraj/models/arti.dart';
-import 'package:book_app_chief_suraj/models/book.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-Color primaryColor = const Color(0xff376565);
-List<BoxShadow> shadowList = [BoxShadow(color: (Colors.grey[400])!, blurRadius: 30, offset: const Offset(0, 10))];
 
-List<ArtiInfo> artis = [
+class List_Arti extends GetxController{
+  List<ArtiInfo> artis = [
   ArtiInfo('Shiv Aarti'.tr,'lib/images/ganpati1.png','lib/assets/hello.md','lib/assets/assets_music_music1.mp3'),
   ArtiInfo('Maa Lakshmi Aarti'.tr,'lib/images/ganpati1.png','lib/assets/lakshmi_aarti.md','lib/assets/assets_music_music1.mp3'),
   ArtiInfo('Maa Saraswati Aarti'.tr,'lib/images/ganpati1.png','lib/assets/saraswati_aarti.md','lib/assets/assets_music_music1.mp3'),
@@ -35,51 +32,4 @@ List<ArtiInfo> artis = [
   ArtiInfo('Maa Ganga Aarti'.tr, 'lib/images/ganpati1.png','lib/assets/ganga_aarti.md','lib/assets/assets_music_music1.mp3'),
 
 ];
-
-List<Map> navList = [
-  {'icon': Icons.phone, 'title': 'Social Media'.tr},
-  {'icon': Icons.markunread_mailbox_rounded, 'title': 'Donation'.tr},
-  {'icon': Icons.add_rounded, 'title': 'Add books'.tr},
-  {'icon': Icons.favorite_rounded, 'title': 'Favorites'.tr},
-  {'icon': Icons.mail_rounded, 'title': 'Messages'.tr},
-  {'icon': Icons.person, 'title': 'Profile'.tr},
-];
-
-List<BookInfo> books = [
-  BookInfo(
-    0,
-    'Ramayaan'.tr,
-    'lib/images/ramayan__1_-removebg-preview.png',
-    'Valmiki'.tr,
-    '250',
-  ),
-  BookInfo(1, 'Mahabharat'.tr, 'lib/images/mahabharata.png', 'Vyasa'.tr, '140'),
-  BookInfo(
-    2,
-    'Gita'.tr,
-    'lib/images/ramayan__1_-removebg-preview.png',
-    'Vyasa'.tr,
-    '290',
-  ),
-  BookInfo(
-    3,
-    'Bharmha puran'.tr,
-    'lib/images/mahabharata.png',
-    'Ramananda'.tr,
-    '159',
-  ),
-  BookInfo(
-    4,
-    'Shiv Puran'.tr,
-    'lib/images/ramayan__1_-removebg-preview.png',
-    'Vyasadeva'.tr,
-    '270',
-  ),
-  BookInfo(
-    5,
-    'Gadur puran'.tr,
-    'lib/images/mahabharata.png',
-    'Vyasadeva'.tr,
-    '160',
-  ),
-];
+}
